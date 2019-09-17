7081204327aebbb81b8d10f9b10676ebe4432d82
Mauricio Frasson Junior <m.frasson.jr@gmail.com>
2019-09-16 22:50:43 -0300

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -11,6 +11,7 @@ import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -90,7 +91,7 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 			st.executeUpdate();
 		}
 		catch(SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatment(st);
